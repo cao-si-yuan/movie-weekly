df9b4c3fef37219edb0d7ff6b9f532fa56468bf9
caosiyuan <17765103160@163.com>
2018-12-02 14:51:14 +0800

    have learned data bind to solve traditional update dom api

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -1,1 +1,11 @@
-Page({})
+Page({
+    data: {
+        thisWeekMovie: {
+            name: "教父",
+            comment:  "最精彩的剧本，最真实的黑帮电影。",
+            imagePath: "/images/jf.jpeg"
+        },
+        count: 123,
+        score: 60
+    }
+})
