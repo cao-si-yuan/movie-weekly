27b728aed789ab83f194e42d1f2b69d234355783
caosiyuan <17765103160@163.com>
2018-12-02 22:36:09 +0800

    movie-weekly completed

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -6,18 +6,21 @@ Page({
                 comment: "失去的才是永恒的",
                 imagePath: "/images/titanic.jpg",
                 isHighlyRecommended: false,
+                id: 1292722
             },
             {
                 name: "这个杀手不太冷",
                 comment: "小萝莉与怪蜀黍纯真灿烂的爱情故事",
                 imagePath: "/images/leon.jpg",
                 isHighlyRecommended: false,
+                id: 1295644
             },
             {
                 name: "教父",
                 comment: "最精彩的剧本，最真实的黑帮电影。",
                 imagePath: "/images/jf.jpeg",
                 isHighlyRecommended: true,
+                id: 1291841
             }
         ],    
         count: 123,
@@ -36,6 +39,20 @@ Page({
         this.setData({
             currentIndex: this.data.weeklyMovieList.length - 1
         })
+    },
+
+    f1: function (event) {
+        var movieId = event.currentTarget.dataset.movieId
+        console.log(movieId);
+        wx.navigateTo({
+            url: '/pages/detail/detail?id=' + movieId
+        })
+    },
+
+    onShareAppMessage: function() {
+        return {
+            title: "每周推荐"
+        }
     }
     //onShow只要显示就调用一次
     // onShow: function() {
